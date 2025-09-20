0fdf309b092dbc3366933822f8078151263577c6
chaiyarerk-hom <chaiyarerk.hom@zohomail.com>
2025-09-20 11:59:06 -0400

    Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,42 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  if (books) {
+    return res.send(books);
+  } else {
+  return res.status(404).json({message: "No available list of books"});
+  }
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    //Write your code here
+    const isbn = req.params.isbn;
+    if (books[isbn]) {
+      return res.send(books[isbn])
+    } else {
+      return res.status(404).json({message: "No available ISBN"});
+    }
+   });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let matchingBooks = [];
+  
+  const keys = Object.keys(books);
+
+  for (let key of keys) {
+    if (books[key].author === author) {
+      matchingBooks.push(books[key]);
+    }
+  }
+  if (matchingBooks) {
+    return res.send(matchingBooks);
+  } else {
+    return res.status(404).json({message: "No available book"});
+  }
 });
 
 // Get all books based on title
