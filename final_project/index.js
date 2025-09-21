80f938ab3c6595f2c8acff57c1d60bcefd522dd5
chaiyarerk-hom <chaiyarerk.hom@zohomail.com>
2025-09-21 13:27:39 -0400

    Task 7 ing

--- final_project/index.js
+++ final_project/index.js
@@ -14,7 +14,7 @@ app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
     if (req.session.authorization) {
         let token = req.session.authorization['accessToken'];
-        jwt.verify(token, "access", (err, user) => {
+        jwt.verify(token, secret, (err, user) => {
             if (!err) {
                 req.user = user;
                 next();
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [{ username:"my_username", password:"my_password" }];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
@@ -17,12 +17,30 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+    for (var i = 0; i < users.length; i++) {
+        if ((users[i].username === username) && (users[i].password === password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    function createToken(payload) {
+        const token = jwt.sign(payload, secret, { expiresIn: '1h' });
+        return token;
+    }
+
+    if (authenticatedUser(username,password)) {
+        const user = users.find(user => user.username === username);
+        const token = createToken({ username: user.username });
+        return res.status(200).json({message: "Successfully registered", token: token});
+    } else {
+        return res.status(401).json({message: "Username or password is invalid"});
+    }
 });
 
 // Add a book review
