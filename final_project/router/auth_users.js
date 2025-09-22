4af6621771e24aea3ed30a9a2d5be4ac4cf92e63
chaiyarerk-hom <chaiyarerk.hom@zohomail.com>
2025-09-22 03:04:42 -0400

    Task 10

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -69,6 +69,18 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   return res.status(200).json({message: "Successfully added reviews", reviews: books[isbn].reviews});
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const user = req.session.authorization["username"];
+
+    if (!books[isbn]) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+    
+    delete books[isbn].reviews[user];
+    return res.status(200).json({message: "Review deleted successfully"});
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const axios = require("axios");
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -19,13 +19,11 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', (req, res) => {
   //Write your code here
-  if (books) {
-    return res.send(books);
-  } else {
-  return res.status(404).json({message: "No available list of books"});
-  }
+  axios.get('https://localhost:5000/books')
+  .then(response => { return res.status(200).json(response.data) })
+  .catch(error => { return res.status(500).json({message: "Error fetching data:", error: error.message}); });
 });
 
 // Get book details based on ISBN
