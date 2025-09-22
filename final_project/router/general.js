2a2eaca700403a416d7d756e36033e0248b36740
chaiyarerk-hom <chaiyarerk.hom@zohomail.com>
2025-09-22 03:43:05 -0400

    Task 14

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,61 +21,38 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/', (req, res) => {
   //Write your code here
-  axios.get('https://localhost:5000/books')
+  axios.get('http://localhost:5000/books')
   .then(response => { return res.status(200).json(response.data) })
-  .catch(error => { return res.status(500).json({message: "Error fetching data:", error: error.message}); });
+  .catch(error => { return res.status(404).json({message: "Error fetching data:", error: error.message}); });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     //Write your code here
     const isbn = req.params.isbn;
-    if (books[isbn]) {
-      return res.send(books[isbn])
-    } else {
-      return res.status(404).json({message: "No available ISBN"});
-    }
-   });
-  
+    axios.get('http://localhost:5000/books' + isbn)
+    .then(response => { return res.status(200).json(response.data) })
+    .catch(error => { return res.status(404).json({message: "Error fetching data:", error: error.message}); });
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   const author = req.params.author;
-  let matchingBooks = [];
-  
-  const keys = Object.keys(books);
-
-  for (let key of keys) {
-    if (books[key].author === author) {
-      matchingBooks.push(books[key]);
-    }
-  }
-  if (matchingBooks) {
-    return res.send(matchingBooks);
-  } else {
-    return res.status(404).json({message: "No available book"});
-  }
+  axios.get('http://localhost:5000/books' + author)
+  .then(response => { return res.status(200).json(response.data) })
+  .catch(error => { return res.status(404).json({message: "Error fetching data:", error: error.message}); });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
 const title = req.params.title;
-  let matchingBooks = [];
-  
-  const keys = Object.keys(books);
-
-  for (let key of keys) {
-    if (books[key].title === title) {
-      matchingBooks.push(books[key]);
-    }
-  }
-  if (matchingBooks) {
-    return res.send(matchingBooks);
-  } else {
-    return res.status(404).json({message: "No available book"});
-  }
+axios.get('http://localhost:5000/books' + title)
+  .then(response => { return res.status(200).json(response.data) })
+  .catch(error => { return res.status(404).json({message: "Error fetching data:", error: error.message}); });
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
